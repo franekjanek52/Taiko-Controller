59ad6b1a0155045af716f42e6bf6bffa04206bc9
Jan Sokołowski <67105352+franekjanek52@users.noreply.github.com>
2024-09-03 22:27:04 +0200

    making code more readable

--- taikov2.ino
+++ taikov2.ino
@@ -9,6 +9,7 @@
 Joystick_ Joystick;     // Create a Joystick object
 
 void setup() {
+  //Serial.begin(9600);
   Joystick.begin(false); //joystick initialisation with auto update turned off
                         // using pinMode is not nessesary because analog pins are inputs by default
 }
@@ -16,67 +17,63 @@ void setup() {
 void loop() {                //DL..DR..KL..KR
  static unsigned int threshold_min[] = {15, 15, 15, 15};// Adjust threshold values based on your sensor readings
  static unsigned int threshold[] = {10, 10, 10, 10};    // Dynamic threshold array
- static unsigned int sensorValue[] = {0, 0, 0, 0};      // recorded values array 
  static unsigned int maxv = 0; 
  static unsigned int id = 0;
  bool activity = false;
- 
-   sensorValue[0] = analogRead(DON_L);   // reading inputs. using analogRead isn`t optimal for this but it works good enough :P 
-   sensorValue[2] = analogRead(KA_L);
-   sensorValue[1] = analogRead(D0N_R);
-   sensorValue[3] = analogRead(KA_R);
-   
 
-maxv = 0;
-for(byte i = 0; i < 4; i++){           // checking for maximum value
+ unsigned int sensorValue[] = {analogRead(DON_L), analogRead(D0N_R), analogRead(KA_L), analogRead(KA_R)}; // reading inputs. using analogRead isn`t optimal for this but it works good enough :P 
+
+ maxv = 0;
+ for(byte i = 0; i < 4; i++){           // checking for maximum value
     if(sensorValue[i] > maxv){
       maxv = sensorValue[i];
       id = i;
       } 
-}
+ }
 
-if(sensorValue[id] > threshold[id]){ // if highest recoeded value is bigger than threshold
-
-      threshold[id] = sensorValue[id]; // Threshold for this input is changed to the same level as its recorded value 
-      Joystick.pressButton(id);        // Coresponding button is pressed
-     
-      if(id == 0){                     // thresholds for other inputs depends on index of triggered input 
-        threshold[1] = threshold_min[1] + sensorValue[id]/2.5; // Threshold for input of the same type (second center drumpad or rim) is lower than threshold for oposing input type
-        threshold[2] = threshold_min[2] + sensorValue[id]/2;   // Threshold for opposing type is set to 1/2 value of triggered input
-        threshold[3] = threshold_min[3] + sensorValue[id]/2;
-        if(sensorValue[1] >= threshold[1]){                    // Checking if the input of the same type is also triggered (Player played big note/double input )
-          Joystick.pressButton(1);                             // Coresponding button is pressed
-        }
+ if(sensorValue[id] > threshold[id]){ // if highest recoeded value is bigger than threshold
+    activity = true; // Input was present 
+    threshold[id] = sensorValue[id]; // Threshold for this input is changed to the same level as its recorded value 
+    Joystick.pressButton(id);        // Coresponding button is pressed
+    switch(id){
+    case(0):
+                         // thresholds for other inputs depends on index of triggered input 
+      threshold[1] = threshold_min[1] + sensorValue[id]/2.5; // Threshold for input of the same type (second center drumpad or rim) is lower than threshold for oposing input type
+      threshold[2] = threshold_min[2] + sensorValue[id]/2;   // Threshold for opposing type is set to 1/2 value of triggered input
+      threshold[3] = threshold_min[3] + sensorValue[id]/2;
+      if(sensorValue[1] >= threshold[1]){                    // Checking if the input of the same type is also triggered (Player played big note/double input )
+        Joystick.pressButton(1);                             // Coresponding button is pressed
       }
-      if(id == 1){
-        threshold[0] = threshold_min[0] + sensorValue[id]/2.5;
-        threshold[2] = threshold_min[2] + sensorValue[id]/2;
-        threshold[3] = threshold_min[3] + sensorValue[id]/2;
-        if(sensorValue[0] >= threshold[0]){
-          Joystick.pressButton(0);
-          }
-       }
-      if(id == 2){
-        threshold[0] = threshold_min[0] + sensorValue[id]/2;
-        threshold[1] = threshold_min[1] + sensorValue[id]/2;
-        threshold[3] = threshold_min[3] + sensorValue[id]/2.5;
-        if(sensorValue[3] >= threshold[3]){
-          Joystick.pressButton(3);
-         }
-      }
-      if(id == 3){
-        threshold[0] = threshold_min[0] + sensorValue[id]/2;
-        threshold[1] = threshold_min[1] + sensorValue[id]/2;
-        threshold[2] = threshold_min[2] + sensorValue[id]/2.5;
-        if(sensorValue[2] >= threshold[2]){
-          Joystick.pressButton(2);
-        }
-      }
+      break;
       
-     activity = true; // Input was present
-     
+    case(1):
+      threshold[0] = threshold_min[0] + sensorValue[id]/2.5;
+      threshold[2] = threshold_min[2] + sensorValue[id]/2;
+      threshold[3] = threshold_min[3] + sensorValue[id]/2;
+      if(sensorValue[0] >= threshold[0]){
+        Joystick.pressButton(0);
+        }
+      break;
+      
+    case(2):
+      threshold[0] = threshold_min[0] + sensorValue[id]/2;
+      threshold[1] = threshold_min[1] + sensorValue[id]/2;
+      threshold[3] = threshold_min[3] + sensorValue[id]/2.5;
+      if(sensorValue[3] >= threshold[3]){
+        Joystick.pressButton(3);
+       }
+      break;
+      
+    case(3):
+      threshold[0] = threshold_min[0] + sensorValue[id]/2;
+      threshold[1] = threshold_min[1] + sensorValue[id]/2;
+      threshold[2] = threshold_min[2] + sensorValue[id]/2.5;
+      if(sensorValue[2] >= threshold[2]){
+        Joystick.pressButton(2);
+      }
+      break;
     }
-  
+ }
  
  if(activity == true){                  // if input was present
    Joystick.sendState();             // updating controller output 
@@ -87,12 +84,11 @@ if(sensorValue[id] > threshold[id]){ // if highest recoeded value is bigger than
    Joystick.sendState();             // updating controller output   
  }
      
- for(byte p = 0; p < 4; p++){           // Subtracting value of decay from tresholds
-    if(threshold[p] > threshold_min[p]){
-      threshold[p] = threshold[p] - decay;
-    }
- }
- 
+  for(byte p = 0; p < 4; p++){           // Subtracting value of decay from tresholds
+     if(threshold[p] > threshold_min[p]){
+       threshold[p] = threshold[p] - decay;
+     }
+  }
 } //end of main loop
 /*
 #=================================================================================#
